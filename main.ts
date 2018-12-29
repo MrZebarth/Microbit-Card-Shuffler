40d62920d796980c70dadff72a1f690f5734adc0
MrZebarth <kris.zebarth@ocsb.ca>
2018-12-28 21:54:19 -0500

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//shuffle the deck
+function shuffle(jokers: boolean): number[] {
+    let deck = [0];
+    deck.length = 0
+
+    //fill a deck of 52 cards
+    for (let x = 0; x < 52; x++) {
+        deck.push(x)
+    }
+    if (jokers) {
+        deck.push(14);
+        deck.push(14);
+    }
+    //shuffle the deck.
+    let shuffleDeck = [0]
+    shuffleDeck.length = 0;
+    for (let x = 0; x < deck.length; x++) {
+        let element = Math.randomRange(0, deck.length - 1)
+        shuffleDeck.push(deck[element])
+        deck.removeAt(element)
+    }
+    deck = shuffleDeck
+    return deck
+}
